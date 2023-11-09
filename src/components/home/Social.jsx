0fb80427ada5b222e995dfb87c1773ab3ed03546
Ybeccc <115051086+Ybeccc@users.noreply.github.com>
2023-11-09 08:46:22 +0700

    First Commit

---
+++ src/components/home/Social.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const Social = () => {
+  return (
+    <div className="home__social">
+        <a href="https://github.com/Ybeccc" className="home__social-icon" target="_blank">
+            <i class="uil uil-github"></i>
+        </a>
+        <a href="" className="home__social-icon" target="_blank">
+            <i class="uil uil-behance-alt"></i>
+        </a>
+        <a href="https://www.linkedin.com/in/ribka-suwardy-349646286/" className="home__social-icon" target="_blank">
+            <i class="uil uil-linkedin"></i>
+        </a>
+    </div>
+  )
+}
+
+export default Social
